852c21904bc4a5d683c919774709607e065e897f
Daniela García <DanyGa02@hotmail.com>
2020-12-18 18:04:19 -0600

    :)

---
+++ ProyectoFinalCsharp/ProyectoFinalCsharp/EstructurasdeDatos/Listas/ListaSimpleCircular/ListaSimpleCircular.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinalCsharp.EstructurasdeDatos.Listas.ListaSimpleCircular
+{
+    public partial class ListaSimpleCircular : Form
+    {
+        public ListaSimpleCircular()
+        {
+            InitializeComponent();
+        }
+    }
+}
